5fd8c6a1d199ae1c1bca18e7bd4da4cc6ae003c3
Dylan Barva <d.barva5402@gmail.com>
2022-05-03 17:15:10 -0700

    finish lab 6

--- app/src/main/java/edu/ucsd/cse110/lab6/Exercises.java
+++ app/src/main/java/edu/ucsd/cse110/lab6/Exercises.java
@@ -1,5 +1,6 @@
 package edu.ucsd.cse110.lab6;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -41,7 +42,11 @@ public class Exercises {
         Pattern re = Pattern.compile("('|'s)?[.,;]?[\\s\\n]", Pattern.MULTILINE);
         Stream<String> wordStream = re.splitAsStream(input);
         Set<String> excludedWords = Constants.excludedWords;
+        List<String> s = wordStream.map(String::toLowerCase)
+                .filter(word -> !excludedWords.contains(word))
+                .filter(word -> word.length() == 6)
+                .collect(Collectors.toList());
+        return s.size();
 
-        return 0;
     }
 }
